6f29d51021f330817ea331528bb61ebe2094231b
lucheng <lucheng@kezaihui.com>
2017-12-01 17:04:26 +0800

    commit

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def getMusic(key):
 @app.route('/music/info', methods=['GET'])
 def getMusicInfo():
     musicInfo = getMusic(request.json['keyword'])
-    return jsonify(musicInfo)
+    return jsonify(url = musicInfo['url'])
 
 
 @app.route('/api/emotionkey', methods=['POST'])
@@ -120,6 +120,6 @@ def hello_world():
 if __name__ == '__main__':
     # print getMusicUrl(u'º£À«Ìì¿Õ')
     # print getMusic('347231')
-    # print getMusic('sad')
+    print getMusic('sad')
     # print getEmotionKey('https://i.pinimg.com/736x/dd/21/a5/dd21a5719f50d914faf50c7b01c00a7f--taylor-marie-hill-taylor-hill-face.jpg')
-    app.run(debug = True)
+    # app.run(debug = True)
