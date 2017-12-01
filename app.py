893b928c894817893c20164f5dce300c223907e4
huangyanyun <huangyanyun@kezaihui.com>
2017-12-01 19:30:49 +0800

    add log

--- app.py
+++ app.py
@@ -23,7 +23,9 @@ cloudinary.config(cloud_name = "k3ith", api_key = "727583551141279", api_secret 
 
 # emotion api
 def getEmotionScore(image):
+    logging.log(logging.DEBUG, "getEmotionScore")
     imageUrl = cloudinary.uploader.upload(image)['secure_url']
+    logging.log(logging.DEBUG, "imageUrl = " + imageUrl)
     headers = {
         'Content-Type': 'application/octet-stream',
         'Ocp-Apim-Subscription-Key': 'db7f098cfc6544f799e25b12c103aa55',
@@ -34,12 +36,15 @@ def getEmotionScore(image):
     conn = httplib.HTTPSConnection('westus.api.cognitive.microsoft.com')
     conn.request("POST", "/emotion/v1.0/recognize?%s" % params, body, headers)
     response = conn.getresponse()
+    logging.log(logging.DEBUG, "response = " + response)
     try:
         data = json.loads(response.read())[0]['scores']
+        logging.log(logging.DEBUG, "data = " + data)
     except Exception:
         print "no face"
     else:
         return data
+        logging.log(logging.DEBUG, "return data = ")
     conn.close()
 
 # get score
